8a604bb8dc4cc058bd12568bb073ca3b83ad610b
Pawel Pacut <pacut.pawel@gmail.com>
2020-12-17 23:37:14 +0100

    Change DECIMAL to REAL for development purposes.

--- orm_tests.py
+++ orm_tests.py
@@ -1,13 +1,18 @@
-from models import Round, Exercise
+from models import Set, Exercise, Base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('sqlite:///:memory:', echo=True)
 
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
 
-first = Round(exercise_name='Bench press', weight= 75, number_of_reps=10, tempo='3010')
-second = Round(exercise_name='Bench press', weight= 80, number_of_reps=8, tempo='3010')
-third = Round(exercise_name='Bench press', weight= 100, number_of_reps=6, tempo='3010')
+session = Session()
 
-print(first)
-print(second)
-print(third)
+
+first = Set(exercise_name='Bench press', weight=75, number_of_reps=10, tempo='3010')
+second = Set(exercise_name='Bench press', weight=80, number_of_reps=8, tempo='3010')
+third = Set(exercise_name='Bench press', weight=100, number_of_reps=6, tempo='3010')
+
+session.add_all([first, second, third])
+session.commit()
